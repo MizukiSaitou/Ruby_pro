ea6043d68763be67038681a3accfce408c0fde22
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2021-08-13 22:39:04 +0900

    Add 5.2

---
+++ chapter5/5.2.rb
@@ -0,0 +1,33 @@
+currencies = {'japan' => 'yen', 'us' => 'doller', 'india' => 'rupee'}
+# イタリアの通過を追加する
+currencies['italy'] = 'euro'
+p currencies
+
+currencies = {'japan' => 'yen', 'us' => 'doller', 'india' => 'rupee'}
+# 既存の値を上書きする
+currencies['japan'] = '円'
+p currencies
+
+currencies = {'japan' => 'yen', 'us' => 'doller', 'india' => 'rupee'}
+p currencies['india']
+
+currencies = {'japan' => 'yen', 'us' => 'doller', 'india' => 'rupee'}
+currencies.each do |key, value|
+  puts "#{key} : #{value}"
+end
+
+a = {'x' => 1, 'y' => 2, 'z' => 3}
+# 全てのキーと値が同じであればtrue
+b = {'x' => 1, 'y' => 2, 'z' => 3}
+p a == b
+# 並び順が異なっていてもキーと値が全て同じならtrue
+c = {'z' => 3, 'y' => 2, 'x' => 1}
+p a == c
+# キー'x'の値が異なるのでfalse
+d = {'x' => 10, 'y' => 2, 'z' => 3}
+p a == d
+
+currencies = {'japan' => 'yen', 'us' => 'doller', 'india' => 'rupee'}
+currencies.delete('japan')
+p currencies
+
