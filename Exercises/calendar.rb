1b9b705e777b18b049ad2fb26d0a3f65bb79d460
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2021-08-15 15:00:57 +0900

    finish calendar

--- Exercises/calendar.rb
+++ Exercises/calendar.rb
@@ -41,7 +41,36 @@ week = this_month.wday
 weeks = ["日", "月", "火", "水", "木", "金", "土"]
 puts "#{weeks[week]}"
 
-# カレンダー作成
+
+
+# 【カレンダー作成】
 first_day = Date.new(2021, 8, 1)
 last_day = Date.new(2021, 8, -1)
+# 1日の曜日を表示
+first_day_week = first_day.wday
+month = first_day.strftime("%B %Y年")
+# %wで文字列の配列を作成する
+week = %w(Su Mo Tu We Th Fr Sa)
+
+# centerメソッドは引数の数が文字数となりそれに沿って中央揃い
+puts month.center(20)
+# weekの,を消して表示
+puts week.join(" ")
+# 1日の曜日までの空白を表示
+print " " * first_day_week
+
+days = (first_day..last_day).to_a
+days.each do |day|
+  # 右寄せする
+  print day.strftime("%d").to_s.rjust(2) + " "
+
+  week_day = first_day_week +=1
+  # スタートした曜日から繰り返し処理で1を足していき7の倍数になったら改行
+  if week_day%7 == 0
+    print "\n"
+  end
+end
+if first_day_week%7 != 0
+  print "\n"
+end
 
